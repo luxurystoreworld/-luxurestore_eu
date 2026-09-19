3846999915bb6a7fbb5119e2e982e2d033faebff
luxurystoreworld <saidalievadilnoza03@gmail.com>
2026-09-19 11:21:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -48,3 +48,60 @@ menu.addEventListener("click",function(){
 nav.classList.toggle("active");
 
 });
+/* =========================
+   LANGUAGE SWITCHER
+========================= */
+
+const language = document.getElementById("language");
+
+function changeLanguage(lang){
+
+document.getElementById("home").textContent =
+translations[lang].home;
+
+document.getElementById("perfumes").textContent =
+translations[lang].perfumes;
+
+document.getElementById("about").textContent =
+translations[lang].about;
+
+document.getElementById("contact").textContent =
+translations[lang].contact;
+
+document.getElementById("heroTitle").textContent =
+translations[lang].heroTitle;
+
+document.getElementById("heroText").textContent =
+translations[lang].heroText;
+
+document.getElementById("shop").textContent =
+translations[lang].shop;
+
+document.getElementById("price").textContent =
+translations[lang].price;
+
+document.getElementById("brands").textContent =
+translations[lang].brands;
+
+document.getElementById("why").textContent =
+translations[lang].why;
+
+localStorage.setItem("language",lang);
+
+}
+
+const savedLanguage = localStorage.getItem("language");
+
+if(savedLanguage){
+
+language.value = savedLanguage;
+
+changeLanguage(savedLanguage);
+
+}
+
+language.addEventListener("change",function(){
+
+changeLanguage(this.value);
+
+});
